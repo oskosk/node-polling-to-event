60fbfb7d6d6891c0f90c0467b04f36b29245222f
osk <oskosk@gmail.com>
2015-02-02 13:11:42 -0300

    Implemented long polling using deep-equal as comparing method

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var extend = require("extend"),
   debug = require("debug")("polling-to-event"),
   util = require("util"),
-  EventEmitter = require("events").EventEmitter;
+  EventEmitter = require("events").EventEmitter,
+  equal = require("deep-equal");
 
 module.exports = pollingtoevent;
 
@@ -10,10 +11,13 @@ function pollingtoevent(func, options) {
     return new pollingtoevent(func, options);
   }
 
+  var lastParams = undefined;
   var _this = this,
     defaults = {
       interval: 1000,
-      eventName: "interval"
+      eventName: "interval",
+      updateEventName: "update",
+      longpolling: false,
     };
 
   // Inherit from EventEmitter
@@ -27,12 +31,32 @@ function pollingtoevent(func, options) {
       return _this.emit("error", err);
     }
     debug("Emitting '%s'.", options.eventName);
+    // Save the event name as first item in the parameters array
+    // that will be used wit _this.emit.apply()
     var params = [options.eventName];
     for (var i = 1; i < arguments.length; i++) {
       params.push(arguments[i]);
     }
-    _this.emit.apply(_this, params);
-    //return _this.emit(options.eventName, data);
+    // If long polling is set, compare
+    // the last value polled with the last one
+    // emit
+    if (options.longpolling) {
+      debug("Comparing last polled parameters");
+      //debug("%j, %j", params, lastParams);
+      if (!equal(params, lastParams)) {
+        debug("Last polled data and previous poll data are not equal. Emitting '%s' event", options.updateEventName);
+        var updateEventParams = params.slice(0);
+        updateEventParams[0] = options.updateEventName;
+        // Emit the update event after longpolling
+        _this.emit.apply(_this, params.slice(1))
+      } else {
+        debug("Last polled data and previous poll data are equal.");
+
+      }
+      lastParams = params.slice(0);
+    }
+    // Emit the interval event after every polling
+    return _this.emit.apply(_this, params);
   }
 
   setInterval(function() {
