2bad5c4133e539692c202c7eaf5266fb55c568a3
joseph burger <joseph.burger@zumepizza.com>
2019-03-18 11:06:43 -0700

    Added Jest & its configs Jest is an MIT licensed testing framework: https://jestjs.io/en/

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clearMocks: true,
+    verbose: false,
+    globals: {
+        window: true,
+        test: true
+    },
+    roots: ['.', './example'],
+    moduleFileExtensions: ['js']
+};
